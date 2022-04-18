9b438548651bb88bb80b4e303bb7be6268ee5c38
Mateusz_Glejzer <mateusz.glejzer@fortedigital.no>
2022-04-18 18:51:06 +0200

    project creation

---
+++ ProjectStore.BootStrapper/Startup.cs
@@ -0,0 +1,38 @@
+namespace ProjectStore.BootStrapper;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration, IWebHostEnvironment hostEnvironment)
+    {
+        Configuration = configuration;
+        HostEnvironment = hostEnvironment;
+    }
+    private IConfiguration Configuration { get; }
+
+    private IWebHostEnvironment HostEnvironment { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+    }
+
+    public void Configure(
+        IApplicationBuilder app,
+        IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+            app.UseHttpsRedirection();
+        }
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseEndpoints(endpoints => endpoints.MapControllers());
+    }
+}
